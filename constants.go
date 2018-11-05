9f55924f5b8039a38542271281ad7e3c7de90b0d
clod16 <c.barberini94@gmail.com>
2018-11-05 15:36:54 +0100

    add access control, first version for putData() and getData()

---
+++ constants.go
@@ -0,0 +1,16 @@
+package main
+
+
+// Utilis values //FIXME!!!!!
+
+const (
+	ROLE = "role"
+	UID = "uid"
+)
+
+
+const (
+
+	CALLER_ROLE_1 = "polimi"
+
+)
--- energyManagementWorkFlow.go
+++ energyManagementWorkFlow.go
@@ -53,34 +53,78 @@ func (t *EnergyManagementWorkFlow) Invoke(stub shim.ChaincodeStubInterface) pb.R
 			return shim.Error(err.Error())
 		}
 
-		/* isEnabled, _, err = isInvokerOperator(stub, callerRole)
+		isEnabled, _, err = isInvokerOperator(stub, callerRole)
 		if err != nil {
 			logger.Error("Error getting attribute info: \n", err.Error())
 			return shim.Error(err.Error())
-		} */
+		} 
 	}
 
 	function, args := stub.GetFunctionAndParameters()
+
+	if (!isEnabled){
+		return shim.Error("Permission denied to access the chaincode!!!\n")
+	}
 
 	if function == "putData" {
 		return t.putData(stub, args)
 	} else if function == "getData" {
 		return t.updateAnalyticsInstances(stub, args)
 	}
-	return shim.Error("Invalid invoke function name")
+	return shim.Error("Invalid invoke function name\n")
 }
 
 
 func (t *EnergyManagementWorkFlow) getData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	logger.Info("EnergyManagementWorkFlow:      getData()\n")
+	if len(args) != 1 {
+		logger.Error("getData() ERROR: need exactly one arguments\n")
+		return shim.Error("getData() ERROR: need exactly two arguments")
+	}
 
-
+	energyManagementKey, err := stub.CreateCompositeKey("POLIMI-Energy-Management", args[0])
+	if err != nil {
+		logger.Error("CreateCompositeKey() ERROR: " err.Error())
+		return shim.Error("CreateCompositeKey() ERROR: " err.Error())
+	}
+	dataBytes, err1 := stub.GetState(energyManagementKey)
+	if err1 != nil {
+		logger.Error("GetState() ERROR: " err1.Error())
+		return shim.Error("GetState() ERROR: " err1.Error())
+	}
+	dataString := string(dataBytes[:])
+	logger.Info("getData(): " +dataString)
+	err2 := stub.SetEvent("getData-Event", dataBytes)
+	if err2 != nil {
+		logger.Error("SetEvent() ERROR: " err2.Error())
+	}
+	return shim.Success(dataBytes)
 
 
 }
 func (t *EnergyManagementWorkFlow) putData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
+	logger.Info("EnergyManagementWorkFlow:      putData()\n")
+	if len(args) != 2 {
+		logger.Error("putData() ERROR: need exactly two arguments \n")
+		return shim.Error("putData() ERROR: need exactly two arguments!! [KEY + PAYLOAD]")
+	}
+	energyManagementKey, err := stub.CreateCompositeKey("POLIMI-Energy-Management", args[0])
+	if err != nil {
+		logger.Error("CreateCompositeKey() ERROR: " err.Error())
+		return shim.Error("CreateCompositeKey() ERROR: " err.Error())
+	}
 
-
+	err1 := stub.PutState(energyManagementKey,  []byte(args[1]))
+	if err1 != nil {
+		logger.Error("PutState() ERROR: " err1.Error())
+		return shim.Error("PutState() ERROR: " err1.Error())
+	}
+	err2 := stub.SetEvent("putData-Event", []byte(args[1]))
+	if err2 != nil {
+		logger.Error("SetEvent() ERROR: " err2.Error())
+	}
+	return shim.Success(nil)
 
 }
 
